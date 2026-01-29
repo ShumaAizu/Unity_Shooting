709e17a446c466b5ae3e11796197d5e2078ff2b3
KajinoHiyori <251312@st.yoshida-g.ac.jp>
2026-01-29 15:31:27 +0900

    BulletController BulletGeneratorを追加

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/BulletGenerator.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletGenerator : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(BulletPrefab);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            bullet.GetComponent<BulletController>().Shoot(worldDir.normalized * 2000);
+        }
+
+        // ESCÉLÅ[Ç≈èIóπ
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+}
