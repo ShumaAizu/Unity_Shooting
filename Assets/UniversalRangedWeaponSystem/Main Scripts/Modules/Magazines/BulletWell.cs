6ee9c50a2361ecb2edec442abf6698110272dd43
obikasouma <251311@st.yoshida-g.ac.jp>
2026-02-05 10:28:45 +0900

    武器アセットの削除、建物アセットの追加

--- Assets/UniversalRangedWeaponSystem/Main Scripts/Modules/Magazines/BulletWell.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-namespace UniversalRangedWeaponSystem
-{
-    [AddComponentMenu("Weapon System/Magazines/Bullet Well", 2)]
-    public class BulletWell : Magazine {}
-}
--- Assets/UniversalRangedWeaponSystem/Main Scripts/Modules/Weapon Logic/SingleFireLogic.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-namespace UniversalRangedWeaponSystem
-{
-    [AddComponentMenu("Weapon System/Weapon Behaviour/Single Fire", 1)]
-    public class SingleFireLogic : WeaponLogic {}
-}
